2c39bcd3d7073a1d5a0c1e7eb3418befab824d8e
IndudiniThennakoon <it21170966@my.sliit.lk>
2022-07-30 22:33:38 +0530

    #commit: type of variable

--- test.ts
+++ test.ts
@@ -9,8 +9,16 @@
 // super type 
 
 let isCheck: any = true // boolean 
-
+console.log(typeof isCheck)
 isCheck = 'True' // assign string 
+console.log(typeof isCheck)
 isCheck = 8 // assign string
+console.log(typeof isCheck)
 isCheck ={id : 1, name: 'Indu'};
+console.log(typeof isCheck)
 console.log(isCheck);
+
+
+
+
+
