15fd70c1668bf9c94c55b584f6da7daeccb25088
IndudiniThennakoon <it21170966@my.sliit.lk>
2022-07-30 22:21:59 +0530

    #commit: supertype any

--- test.ts
+++ test.ts
@@ -1,6 +1,17 @@
-let indu: string = "Hello Indu";
+// let indu: string = "Hello Indu";
 
-let induu: number = 5;
+// let induu: number = 5;
 
-console.log(indu);
-console.log(induu);
+// console.log(indu);
+// console.log(induu);
+
+
+// super type 
+
+let isCheck: any = true;
+
+isCheck = 'True';
+isCheck = 0
+isCheck ={id : 1, name: 'Indu'};
+
+console.log(isCheck);
