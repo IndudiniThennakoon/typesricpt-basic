36e8be3663ee4b8defbb94828be77ba5d42e0377
IndudiniThennakoon <it21170966@my.sliit.lk>
2022-07-30 23:24:34 +0530

    # add compiled js file

--- test.js
+++ test.js
@@ -1,13 +1,8 @@
 // let indu: string = "Hello Indu";
-
 // let induu: number = 5;
-
 // console.log(indu);
 // console.log(induu);
-
-
 // super type 
-
 // let isCheck: any = true // boolean 
 // console.log(typeof isCheck)
 // isCheck = 'True' // assign string 
@@ -17,78 +12,56 @@
 // isCheck ={id : 1, name: 'Indu'};
 // console.log(typeof isCheck)
 // console.log(isCheck);
-
-
 //  array 
-
 // const list: number [] = [1,2,3,4];
 // console.log(list);
-
 // tuple
 // let x: [number,string];
 // x = [112, 'Hello'];
-
 // console.log(x);
-
 //  enum
 // enum ActionTypes{
 //     GET_PRODUCTS = '[GET PRODUCTS] LOADED',
 //     CREATE_PRODUCTS = '[CREATE PRODUCTS] CREATE'
 // }
-
 // const action = {
 //     payload = 1,
 //     type: ActionTypes.GET_PRODUCTS
 // }
 // console.log(action);
-
 // unknown variable
-
 // let notSure: unknown = 4;
 // notSure = 'maybe a string instead';
-
 // notSure = false;
 // console.log(notSure);
-
 //  function
-
 // function test() : void {
 //     console.log('Hi')
 // }
 // test();
-
 // let value = undefined;
 // value = 5;
 // console.log(value);
-
 // let result = null;
 // result ={ id : 1, name : 'All'};
 // console.log(result);
-
 // never
 // function error (message: string): never{
 //     throw new Error (message);
 // }
-
 // function fail(){
 //     return error("something failed")
 // }
-
 // object
-
 // let employee: object = { id : 0, name : ''};
 // employee = {
 //     id: 1,
 //     name: 'Indu'
 // }
 // console.log(employee);
-
-
 // arithmetic operators
-
 // let a = 2;
 // let b = 3;
-
 // let value = a + b;
 // console.log(value);
 // console.log(a - b);
@@ -96,21 +69,17 @@
 // console.log(a / b);
 // console.log(a ++);
 // console.log(a--);
-
 //  relational operators
-
 // console.log(a < b);
 // console.log(a > b);
 // console.log(a <= b);
 // console.log(a >= b);
-
 //  logical operators
 // let c = 3;
 // let d = 10;
 // console.log(a < b && c < d);
 // console.log(!(a<b) );
 // console.log(a < b && c < d);
-
 //  if
 // let value = 'STRING VALUE';
 // if (value == 4)
@@ -120,46 +89,27 @@
 //     console.log("value == 5");
 // }else
 // console.log("Falsy");
-
 // switch
-
 // let c = 3
 // switch(c){
 //     case 1: console.log('Value is 1 ');
 //     break;
-
 //     case 2: console.log('Value is 2 ');
 //     break;
-
 //     case 3: console.log('Value is 3 ');
 //     break;
-
 //     default: console.log('invalid');
 // }
-
 //  for loop
-
 // let count = 10;
 // for(let i = 0 ; i < count ; i++){
 //     console.log(i);
 // }
-
 //  while loop
-
 // let num = 5;
-
 // let factorial : number = 1;
-
 // while(num >=1){
 //     factorial = factorial * num;
 //     num --;
 // }
-
 // console.log(factorial);
-
-
-
-
-    
-
-
